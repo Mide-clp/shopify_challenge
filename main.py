0bf193fd9b8f9aa93272427af067b3654d209719
Mide clp <aogunnola@gmail.com>
2022-05-18 18:01:26 +0100

    update database to postgres

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
 load_dotenv()
-BUCKET = os.getenv("BUCKET")
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-REGION_NAME = os.getenv("REGION_NAME")
+BUCKET = os.environ.get("BUCKET")
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+REGION_NAME = os.environ.get("REGION_NAME")
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "letbuildthisstuff"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///images.db"
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL",  "sqlite:///images.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 db = SQLAlchemy(app)
